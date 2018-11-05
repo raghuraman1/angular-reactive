d0ec9f5e12dfa726d0042d9dc825c97f327b5ce3
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2018-11-05 19:24:40 +0530

    saved

--- src/app/profile-editor-fb/profile-editor-fb.component.ts
+++ src/app/profile-editor-fb/profile-editor-fb.component.ts
@@ -10,9 +10,12 @@ import { forbiddenNameValidator } from './forbidden-name.directive';
   styleUrls: ['./profile-editor-fb.component.css']
 })
 export class ProfileEditorFBComponent implements OnInit {
+  controlFieldCtrl = this.fb.control('', Validators.required);
+
   profileForm = this.fb.group({
+    controlField: this.controlFieldCtrl,
     firstName: ['', Validators.required],
-    lastName: ['', forbiddenNameValidator('bob') ],
+    lastName: ['', forbiddenNameValidator('bob')],
     address: this.fb.group({
       street: [''],
       city: [''],
@@ -26,41 +29,58 @@ export class ProfileEditorFBComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-  }
-
-  get aliases() {
-    return this.profileForm.get('aliases') as FormArray;
-  }
-
-  addAlias() {
-    this.aliases.push(this.fb.control(''));
-  }
-
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(this.profileForm.value);
-  }
-
-  isFieldValid(field: string) {
-    return !this.profileForm.get(field).valid ;
-  }
-
-  
-  displayFieldCss(field: string) {
-    return {
-      'has-error': this.isFieldValid(field),
-      'has-feedback': this.isFieldValid(field)
-    };
-  }
-  
-
-  updateProfile() {
-    this.profileForm.patchValue({
-      firstName: 'Nancy',
-      lastName: 'bob',
-      address: {
-        street: '123 Drew Street'
-      }
+    this.controlFieldCtrl.statusChanges.subscribe((value: string) => {
+this.enableDisable();
     });
+    this.enableDisable();
   }
+
+  
+
+get aliases() {
+  return this.profileForm.get('aliases') as FormArray;
+}
+
+enableDisable()
+{
+  if(this.controlFieldCtrl.valid)
+  {
+   this.profileForm.controls.firstName.enable();
+  }
+  else
+  {
+    this.profileForm.controls.firstName.disable();
+  }
+}
+addAlias() {
+  this.aliases.push(this.fb.control(''));
+}
+
+onSubmit() {
+  // TODO: Use EventEmitter with form value
+  console.warn(this.profileForm.value);
+}
+
+isFieldValid(field: string) {
+  return !this.profileForm.get(field).valid;
+}
+
+/* what about this */
+displayFieldCss(field: string) {
+  return {
+    'has-error': this.isFieldValid(field),
+    'has-feedback': this.isFieldValid(field)
+  };
+}
+
+
+updateProfile() {
+  this.profileForm.patchValue({
+    firstName: 'Nancy',
+    lastName: 'bob',
+    address: {
+      street: '123 Drew Street'
+    }
+  });
+}
 }
